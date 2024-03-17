23fa0d87b02aa059b7c766f9a735d19139692716
Gabriel Mathias <gablemathias@icloud.com>
2024-03-18 10:16:20 +1100

    Exercises 1.1 and 1.2

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,3 +1,37 @@
+// Header takes care of rendering the name of the course,
+// Content renders the parts and their number of exercises and
+// Total renders the total number of exercises.
+
+const Header = (props) => {
+  return (
+    <h1> {props.course} </h1>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <p>{props.part} {props.exercise}</p>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <div>
+      <Part part={props.part[0]} exercise={props.exercise[0]}/>
+      <Part part={props.part[1]} exercise={props.exercise[1]}/>
+      <Part part={props.part[2]} exercise={props.exercise[2]}/>
+    </div>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <p>Number of exercises {props.total}</p>
+  )
+}
+
+
+
 const App = () => {
   const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
@@ -9,17 +43,9 @@ const App = () => {
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header course={course}/>
+      <Content part={[part1, part2, part3]} exercise={[exercises1,exercises2, exercises3]} />
+      <Total total={exercises1 + exercises2 + exercises3} />
     </div>
   )
 }
